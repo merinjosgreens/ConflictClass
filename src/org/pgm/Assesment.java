720c86f9838354254121cf90924bdfc7c38bc1f7
merinjosgreens <merinrajgreens@gmail.com>
2022-07-13 14:40:56 +0530

    Azar

---
+++ src/org/pgm/Assesment.java
@@ -0,0 +1,14 @@
+package org.pgm;
+
+public class Assesment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+	}
+
+}
